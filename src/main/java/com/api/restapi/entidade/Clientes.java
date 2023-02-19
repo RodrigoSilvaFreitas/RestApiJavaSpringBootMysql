f786a9247eb99070ab2add309f9d0bd31e1d4738
RodrigoSilvaFreitas <rod.s.f@hotmail.com>
2023-02-19 12:26:07 -0300

    Add Lombok

--- src/main/java/com/api/restapi/entidade/Clientes.java
+++ src/main/java/com/api/restapi/entidade/Clientes.java
@@ -5,7 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 public class Clientes {
     @Id
@@ -21,44 +25,4 @@ public class Clientes {
     private String telefone;
     @Column(nullable = false)
     private String endereco;
-    
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getCpf() {
-        return cpf;
-    }
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getTelefone() {
-        return telefone;
-    }
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-    public String getEndereco() {
-        return endereco;
-    }
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-
-    
 }
